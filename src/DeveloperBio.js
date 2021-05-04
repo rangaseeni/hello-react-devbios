43cb180fc4aaa7c0cb5fc064cd73193dcf6816c6
M_688776 <jason.monroe@cognizant.com>
2021-05-04 10:01:11 -0500

    initial

---
+++ src/DeveloperBio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function DeveloperBio() {
+    return (
+        <div className="card d-inline-block text-dark">
+            <div className="card-body">
+                <h5 className="card-title">Jason  Monroe's Bio</h5>
+                <p className="card-text">
+                    <span className="bold">Favorite Language:</span> JavaScript
+                </p>
+                <p className="card-text">
+                    <span className="bold">Year Started:</span> 1999
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default DeveloperBio;
