34a1c9fc43013b145004c5f5a2dd20544b695c09
M_688776 <jason.monroe@cognizant.com>
2021-05-06 09:16:05 -0500

    AddDeveloper Component Form added

---
+++ src/AddDeveloper.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class AddDeveloper extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Add Developer Bio</h1>
+                <div className="row">
+                    <div className="col-mid-6">
+                        <form id="devForm">
+                            <div className="form-group">
+                                <label htmlFor="firstName">First Name</label>
+                                <input type="text" name="firstName"  className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="lastName">Last Name</label>
+                                <input type="text" name="lastName" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="favoriteLanguage">Favorite Language</label>
+                                <input type="text" name="favoriteLanguage"  className="form-control"  />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="yearStarted">Year Started</label>
+                                <input type="text" name="yearStarted"  className="form-control"  />
+                            </div>
+                            <div className="form-group">
+                                <button type="submit" className="btn btn-success" >Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AddDeveloper;
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import DeveloperBio from './DeveloperBio';
 import DisplayBios from './DisplayBios';
+import AddDeveloper from './AddDeveloper';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           Learn React
         </a>
        <DisplayBios />
+       <AddDeveloper />
       </header>
     </div>
   );
